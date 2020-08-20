3a720db833e20cc1a6a6b55cad19d210f3fece21
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-08-20 15:37:01 -0700

    Final correction

--- compare_execute.c
+++ compare_execute.c
@@ -41,41 +41,31 @@ int comparing(char **tokens)
 int executing(char **tokens, char *line)
 {
 	pid_t pid;
-	int o;
-	int status;
+	int o, status;
 	char path[8192] = "/bin", *aux = "/b", *aux2 = " /b.h";
 
 	if (line[0] == aux[0] && line[1] == aux[1])
-	{
 		_strcpy(path, line);
-	}
 	else if (line[0] == aux2[0])
 	{
-		for(o=0;line[o] != '\0';o++)
+		for (o = 0; line[o] != '\0'; o++)
 		{
 			if (line[o] == aux2[1])
-				if (line[o-1] == aux2[0])
-				        _strcpy(path, "/bin/ls");
+				if (line[o - 1] == aux2[0])
+					_strcpy(path, "/bin/ls");
 		}
 	}
 	else if (line[0] == aux2[3] && line[2] == aux2[4])
-	{
 		_strcpy(path, "/bin/ls");
-	}
 	else
-	{
-		_strcat(path, "/");
-		_strcat(path, line);
-	}
-
+		_strcat(path, "/"), _strcat(path, line);
 	pid = fork();
 	if (pid == 0)
 	{
 		if (execve(path, tokens, environ) == -1)
 		{
 			errors(tokens);
-			return (0);
-		}
+			return (0); }
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
